9670bc1156b3379227f72ad4337a44c5e4b29ef2
dilip0605 <dilipmb7475@gmail.com>
2020-08-27 20:41:18 +0530

    first commit

---
+++ BiPC Junior/js/question.js
@@ -0,0 +1,33 @@
+
+
+
+const quiz = [
+
+{
+	q:'<img src="screens/1.PNG"/>'	,
+	options:['1','2','3','4'],
+	answer:1
+},
+{
+	q:'<img src="screens2.PNG"/>'	,
+	options:['1','2','3','4'],
+	answer:0
+},
+{
+	q:'<img src="screens/3.PNG"/>'	,
+	options:['1','2','3','4'],
+	answer:3
+},
+{
+	q:'<img src="screens/4.PNG"/>'	,
+	options:['1','2','3','4'],
+	answer:2
+},
+{
+	q:'<img src="screens/5.PNG"/>'	,
+	options:['1','2','3','4'],
+	answer:1
+}
+//5 questions
+
+]
